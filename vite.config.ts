ab959c1cafccb728e1f3cbdeda2d262ddcd0635d
Maxim Lyubimov <52247631+mlyubimov@users.noreply.github.com>
2025-06-10 19:26:42 +0400

    chore: Update configurations and add typedock and openapi

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import dts from "vite-plugin-dts";
-// import { resolve } from "path";
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
     dts({
       entryRoot: "src",
-      outDir: "dist/types",
+      outDir: "dist",
+      rollupTypes: true,
     }),
   ],
   build: {
     lib: {
-      // entry: resolve(__dirname, "src/index.ts"),
       entry: "src/index.ts",
       name: "busylib",
       fileName: "index",
@@ -27,11 +26,4 @@ export default defineConfig({
     },
     minify: false,
   },
-  // resolve: {
-  //   alias: {
-  //     src: resolve(__dirname, "src"),
-  //     api: resolve(__dirname, "src/api"),
-  //     types: resolve(__dirname, "src/types"),
-  //   },
-  // },
 });
